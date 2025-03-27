b79558ced21831a3952e4d5d9e60231375e31ca8
EATSTEAK <me@eatsteak.dev>
2025-03-27 22:46:04 +0900

    feat: implement rss write

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,53 @@
-use std::path::Path;
+use std::{io::BufWriter, path::Path, sync::Arc};
 
-use ssufid::{core::SsufidCore, plugins::ssu_catch::SsuCatchPlugin};
+use futures::future::join_all;
+use ssufid::{
+    core::{SsufidCore, SsufidPlugin},
+    plugins::ssu_catch::SsuCatchPlugin,
+};
 use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let core = SsufidCore::new("./.ssufid/cache");
+    color_eyre::install()?;
 
-    let site = core
-        .run(SsuCatchPlugin, SsufidCore::POST_COUNT_LIMIT)
-        .await?;
+    let core = Arc::new(SsufidCore::new("./.ssufid/cache"));
 
-    let out_dir = Path::new("./out/example");
-    tokio::fs::create_dir_all(out_dir).await?;
+    let out_dir = Path::new("./out");
 
-    let mut example_file = tokio::fs::File::create_new(out_dir.join("data.json")).await?;
-    // let mut example_rss_file = tokio::fs::File::create_new(out_dir.join("rss.xml")).await?;
-    let example_json = serde_json::to_string_pretty(&site)?;
-    // let example_rss = site.to_rss()?;
-    // TODO: Write rss structure to xml
-    example_file.write_all(example_json.as_bytes()).await?;
+    let tasks = vec![save_run(core.clone(), out_dir, SsuCatchPlugin)];
+
+    join_all(tasks).await.into_iter().for_each(|result| {
+        if let Err(err) = result {
+            eprintln!("{:?}", err);
+        }
+    });
 
     core.save_cache().await?;
+    Ok(())
+}
+
+async fn save_run<T: SsufidPlugin>(
+    core: Arc<SsufidCore>,
+    base_out_dir: &Path,
+    plugin: T,
+) -> eyre::Result<()> {
+    let site = core.run(plugin, SsufidCore::POST_COUNT_LIMIT).await?;
+    let json = serde_json::to_string_pretty(&site)?;
+
+    // Use synchronous BufWriter to write pretty xml string.
+    let buf = site
+        .to_rss()
+        .pretty_write_to(BufWriter::new(Vec::new()), b' ', 2)?;
+    let rss = String::from_utf8(buf.into_inner()?)?;
+
+    let out_dir = base_out_dir.join(T::IDENTIFIER);
+    tokio::fs::create_dir_all(&out_dir).await?;
+
+    let mut json_file = tokio::fs::File::create(out_dir.join("data.json")).await?;
+    json_file.write_all(json.as_bytes()).await?;
+
+    let mut rss_file = tokio::fs::File::create(out_dir.join("rss.xml")).await?;
+    rss_file.write_all(rss.as_bytes()).await?;
     Ok(())
 }
