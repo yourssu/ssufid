8cc6bdf25d976ec29a4e3deae9fc8556f9feebd2
cometj03 <harryjung1001@gmail.com>
2025-04-01 18:28:58 +0900

    chore: rollback main

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,7 @@ async fn save_run<T: SsufidPlugin>(
     base_out_dir: &Path,
     plugin: T,
 ) -> eyre::Result<()> {
-    // let site = core.run(plugin, SsufidCore::POST_COUNT_LIMIT).await?;
-    let site = core.run(plugin, 1).await?;
+    let site = core.run(plugin, SsufidCore::POST_COUNT_LIMIT).await?;
     let json = serde_json::to_string_pretty(&site)?;
 
     // Use synchronous BufWriter to write pretty xml string.
